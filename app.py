1fb5cf136eeb406d10cd21812e30222cb5a1349a
Srishti <srishtimini14@gmail.com>
2026-01-21 16:49:00 +0530

    add comments

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+
+# route for home page, renders form to input name
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
 
+# route to handle form, captures the name and displays welcome msg
 @app.route('/welcome', methods=['POST'])
 def welcome():
     name = request.form.get('name')
